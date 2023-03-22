afb7a2d0dbf83648609cc517f9435f0c3ebc7c1e
imprisoner <continentalresident@yandex.ru>
2023-03-22 23:45:24 +0400

    minor fix

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -75,67 +75,6 @@ modal();
 
 // animating dynamic height TODO
 
-document.addEventListener("DOMContentLoaded", () => {
-  // observe text in cases section
-  const nodes = [".s-cases", ".s-service"].map((sel) =>
-    document.querySelector(sel)
-  );
-  nodes.forEach((node) => {
-    // set initial height
-    node.style.height = node.offsetHeight + "px";
-    node.style.transition = "height 0.8s";
-
-    const observer = new MutationObserver((mr) => {
-      if (node.scrollHeight > node.offsetHeight) {
-        return (node.style.height = node.scrollHeight + "px");
-      }
-
-      node.style.height = node.firstElementChild.offsetHeight + "px";
-    });
-
-    observer.observe(node, {
-      childList: true,
-      subtree: true,
-      characterData: true,
-    });
-  });
-  // nodes.forEach((node) => {
-  //   // set initial height
-  //   node.style.height = node.offsetHeight + "px";
-  //   node.style.transition = "height 0.8s";
-
-  //   // set height:"fit-content" to childNodes
-  //   node.childNodes.forEach((child) => {
-  //     if (child.nodeType !== 3) {
-  //       child.style.height = "fit-content";
-  //     }
-  //   });
-
-  //   // set observable
-
-  //   const observer = new MutationObserver((mr) => {
-  //     // const [record] = mr
-  //     if (node.offsetHeight < node.scrollHeight) {
-  //       console.log("It's bigger!");
-  //       return (node.style.height = node.scrollHeight + "px");
-  //     }
-
-  //     // find the biggest value of childNodes heights
-  //     const childHeights = Array.from(node.childNodes.filter(child => child.nodeType !== 3))
-  //       .map((child) => child.offsetHeight)
-  //       .sort((a, b) => a - b);
-  //     // assign biggest value to our node
-  //     node.style.height = childHeights[childHeights.length - 1] + "px";
-  //   });
-
-  // observer.observe(node, {
-  //   childList: true,
-  //   subtree: true,
-  //   characterData: true,
-  // });
-  // });
-});
-
 const optionsWrap = document.querySelector(".s-service__options");
 const tabs = document.querySelector(".s-service__menu");
 
@@ -158,7 +97,6 @@ tabs.addEventListener("click", () => {
   setTimeout(() => {
     const optionsWrap = document.querySelector(".s-service__options");
     
-    
     if (optionsWrap.offsetHeight < optionsWrap.scrollHeight) {
       optionsWrap.style.height = optionsWrap.scrollHeight + "px";
     } else {
@@ -166,10 +104,12 @@ tabs.addEventListener("click", () => {
       const [height] = getChildren()
       .map((child) => child.offsetHeight)
       .sort((a, b) => b - a);
+
       if (initialHeight > height) {
         optionsWrap.style.height = initialHeight + "px";
         return;
       }
+
       optionsWrap.style.height = height + "px";
     }
   })
